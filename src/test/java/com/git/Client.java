50f22a8159dd0ca4d4f89d00dbd160c6eeb0493a
IswaryaGounassegarane <ishugounse96@gmail.com>
2022-11-06 21:11:08 +0530

    Client class is pushed

---
+++ src/test/java/com/git/Client.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class Client {
+
+	public void clientId() {
+		System.out.println("clientId");
+
+	}
+	public void clientName() {
+		System.out.println("clientName");
+	}
+	private void clientSalary() {
+	System.out.println("salary");
+	}
+}
